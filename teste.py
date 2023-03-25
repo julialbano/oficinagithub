29e8440be58692f9568e4053a8bbfa709843e8b2
julialbano <contato.julialbano@gmail.com>
2023-03-25 09:57:37 -0300

    commit inicial

---
+++ teste.py
@@ -0,0 +1,13 @@
+
+num= int(input('Digite um número:'))
+tab=1
+
+while num<0 or num>=1:
+    while tab<=10:
+        print(num,'x',tab,'=',num*tab)
+        tab=tab+1
+    num= int(input('Digite um número:'))
+    tab=1
+print('Não tem tabuada do 0')
+
+
